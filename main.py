6c9cdc122a21f5a44087e518c9a79e25eb996904
mrctito <mrctito.com>
2024-02-22 21:01:30 -0300

    ok

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import os
 import json
+from fastapi import FastAPI
+from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+import uvicorn
 from llm import prepara_llm
 from prompt import TABELA_COMANDOS_STR, prepara_prompt
 
+app = FastAPI()
 
 def test():
   prompt = prepara_prompt()
@@ -18,13 +22,18 @@ def test():
       print("Comando:"+result["text"])
 
 
-def obtem_comando_menu(texto_usuario: str) -> str:
+class UsuarioInput(BaseModel):
+    texto_usuario: str
+
+@app.post("/obtem_comando_menu/")
+def obtem_comando_menu(usuario_input: UsuarioInput) -> dict:
   prompt = prepara_prompt()
   llm = prepara_llm(prompt)
-  result = llm.invoke({"texto": texto_usuario, "tabela": TABELA_COMANDOS_STR})
+  result = llm.invoke({"texto": usuario_input, "tabela": TABELA_COMANDOS_STR})
   print("Comando:"+result["text"])
   return result["text"]
 
-
-test()
-   
+  
+if __name__ == "__main__":
+  #test()
+  uvicorn.run("main:app", host="0.0.0.0", port=8106)
