97664250aabed70fdcb8c1341e7211fc0446b95d
mrctito <mrctito.com>
2024-10-14 17:47:02 -0300

    OK

--- llm.py
+++ llm.py
@@ -6,7 +6,8 @@ from langchain.chains import LLMChain
 
 
 def cria_llm_openai(verbose: bool = False):
-    llm = ChatOpenAI(temperature=0, verbose=verbose, model=os.getenv("MODEL_NAME"))
+    llm = ChatOpenAI(temperature=0, verbose=verbose,
+                     model=os.getenv("MODEL_NAME"))
     return llm
 
 
@@ -15,17 +16,17 @@ def cria_llm_azure(verbose: bool = False):
     AZURE_OPENAI_API_VERSION = os.getenv("AZURE_OPENAI_API_VERSION")
     AZURE_OPENAI_API_BASE = os.getenv("AZURE_OPENAI_API_BASE")
     AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
-    AZURE_OPENAI_API_TYPE, = os.getenv("AZURE_OPENAI_API_TYPE")
+    AZURE_OPENAI_API_TYPE = os.getenv("AZURE_OPENAI_API_TYPE")
 
     llm = AzureChatOpenAI(
         deployment_name=AZURE_DEPLOYMENT_NAME,
         openai_api_version=AZURE_OPENAI_API_VERSION,
-        openai_api_base=AZURE_OPENAI_API_BASE,
+        azure_endpoint=AZURE_OPENAI_API_BASE,
         openai_api_key=AZURE_OPENAI_API_KEY,
         openai_api_type=AZURE_OPENAI_API_TYPE,
         temperature=0,
         verbose=verbose
-    ) 
+    )
     return llm
 
 
@@ -37,7 +38,7 @@ def cria_llm(verbose: bool = False):
     return llm
 
 
-def cria_chain(prompt: PromptTemplate, verbose: bool = False, llm = None):
+def cria_chain(prompt: PromptTemplate, verbose: bool = False, llm=None):
     if llm is None:
         llm = cria_llm()
     chain = LLMChain(llm=llm, prompt=prompt, verbose=verbose)
--- main.py
+++ main.py
@@ -12,64 +12,68 @@ from prompt import TABELA_COMANDOS_EMPORIO_STR, prepara_prompt
 app = FastAPI()
 
 # rotina para testar o serviço
-def test():
-  prompt = prepara_prompt()
-  llm = cria_chain(prompt)
-  while True:
-      print("\n")
-      texto_usuario = input("Digite o comando desejado: ")
-      if texto_usuario == ".":
-        break
 
-      usuario_input = UsuarioInput(codigo_sistema="EMPORIO", texto_usuario=texto_usuario)
-      result = obtem_comando_menu(usuario_input)
-      print(result)
+
+def test():
+    prompt = prepara_prompt()
+    llm = cria_chain(prompt)
+    while True:
+        print("\n")
+        texto_usuario = input("Digite o comando desejado: ")
+        if texto_usuario == ".":
+            break
+
+        usuario_input = UsuarioInput(
+            codigo_sistema="EMPORIO", texto_usuario=texto_usuario)
+        result = obtem_comando_menu(usuario_input)
+        print(result)
 
 
 class UsuarioInput(BaseModel):
-  codigo_sistema: str
-  texto_usuario: str
+    codigo_sistema: str
+    texto_usuario: str
 
 # serviço que recebe um comando do usuário e retorna o código de menu correspondente
+
+
 @app.post("/obtem_comando_menu/")
 def obtem_comando_menu(usuario_input: UsuarioInput) -> str:
-  prompt = prepara_prompt()
+    prompt = prepara_prompt()
 
-  chain = cria_chain(prompt, verbose=True)
+    chain = cria_chain(prompt, verbose=True)
 
-  # decide qual tabela de comandos usar
-  tabela = ""
-  if usuario_input.codigo_sistema == "EMPORIO":
-    tabela = TABELA_COMANDOS_EMPORIO_STR
-  else:
-    return "0"
+    # decide qual tabela de comandos usar
+    tabela = ""
+    if usuario_input.codigo_sistema == "EMPORIO":
+        tabela = TABELA_COMANDOS_EMPORIO_STR
+    else:
+        return "0"
 
-  # chama a API que traduz o comando do usuario em um código de menu
-  result = chain.invoke({"texto": usuario_input, "tabela": tabela})
-  return result["text"]
+    result = chain.invoke({"texto": usuario_input, "tabela": tabela})
+    return result["text"]
 
-  
+
 if __name__ == "__main__":
 
-  modo_teste = os.getenv("MODO_TESTE", "N")
-  if modo_teste == "S":
-    # se modo teste ativo, então roda test()
-    test()
-  else:
-    # senão coloca o serviço no ar
-    print()
-    print("Iniciando servidor...")
-    print()
-    print("Acesse http://localhost:8106/docs para Swagger")
-    print()
-    print("Para testar, use o comando abaixo")
-    print('curl -X "POST" "http://localhost:8106/obtem_comando_menu/" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"texto_usuario\": \"novo produto\"}"')
-    print()
-    uvicorn.run("main:app", host="0.0.0.0", port=8106)
+    modo_teste = os.getenv("MODO_TESTE", "N")
+    if modo_teste == "S":
+        # se modo teste ativo, então roda test()
+        test()
+    else:
+        # senão coloca o serviço no ar
+        print()
+        print("Iniciando servidor...")
+        print()
+        print("Acesse http://localhost:8106/docs para Swagger")
+        print()
+        print("Para testar, use o comando abaixo")
+        print(
+            'curl -X "POST" "http://localhost:8106/obtem_comando_menu/" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"texto_usuario\": \"novo produto\"}"')
+        print()
+        uvicorn.run("main:app", host="0.0.0.0", port=8106)
 
 # para testar
 # curl -X "POST" "http://localhost:8106/obtem_comando_menu/" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"texto_usuario\": \"novo produto\"}"
-
 
 
 """
